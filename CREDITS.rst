e2343ddcf8a292374f2999387a121c174d704f6b
Henri Hulski <henri@openhomeo.info>
2016-01-20 00:31:35 +0100

    Update README.md and package meta data.

---
+++ CREDITS.rst
@@ -0,0 +1,25 @@
+CREDITS
+=======
+
+* molokov: Initial author
+
+*  Henri Hulski:
+
+   - Rewriting the app to match the new v.zero API.
+   - Add client-side card validation.
+   - Refactor country support.
+   - Include PayPal payments.
+   - Make it a full PyPI package.
+
+*  Jonathan D. Johnson:
+
+   Cartridge_braintree is using the jQuery plugin from payform_
+   for client-side credit card validation and number formatting.
+   
+   .. _payform: https://github.com/jondavidjohn/payform
+
+*  Chris Beaven:
+
+   The country selection is based on django-countries_.
+   
+   .. _django-countries: https://github.com/SmileyChris/django-countries
--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
-import setuptools
+import io
+from setuptools import setup, find_packages
+long_description = (
+    io.open('README.rst', encoding='utf-8').read()
+    + '\n' +
+    io.open('CHANGES.rst', encoding='utf-8').read()
+    + '\n' +
+    io.open('CREDITS.rst', encoding='utf-8').read()
+)
 
-setuptools.setup(
+setup(
     name='cartridge_braintree',
-    packages=setuptools.find_packages(),
-    install_requires=['braintree'],
+    version='1.0b1',
+    description="Braintree Payments processing for Mezzanine/Cartridge",
+    long_description=long_description,
+    maintainer="Henri Hulski",
+    maintainer_email="henri@openhomeo.info",
+    license="MIT",
+    url="https://github.com/henri-hulski/cartridge_braintree",
+    packages=find_packages(),
+    install_requires=[
+        'braintree',
+    ],
+    extras_require=dict(
+        countries_utf8_sorting=['pyuca'],
+    ),
 )
